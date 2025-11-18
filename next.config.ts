523b6fcb8f9063b1e63017216aad11d1ea6e666b
Mathieu Bayou <mathieu@novasquare.io>
2025-11-18 17:41:53 +0100

    Fix: Build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingIncludes: {
+      "/**": ["./src/generated/prisma/**/*"],
+    },
+  },
 };
 
 export default nextConfig;
